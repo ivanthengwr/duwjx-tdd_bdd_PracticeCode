9d2c6d92a69598f5e46e4b209c6835ab2425fd03
Ivan Theng <ivanthengwr@gmail.com>
2023-12-14 18:42:33 +0800

    Learning assert module with nosetests

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,18 +14,26 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(10)
+        self.assertEqual(self.stack.peek(),10)
+        self.stack.push(20)
+        self.assertEqual(self.stack.pop(),20)
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(5)
+        self.stack.push(3)
+        self.assertEqual(self.stack.pop(),3)
+        self.assertEqual(self.stack.peek(),5)
+        self.stack.pop()
+        self.assertTrue(self.stack.is_empty())
 
     def test_peek(self):
         """Test peeking at the top the stack"""
         self.stack.push(5)
         self.assertTrue(isinstance(self.stack.items[0], int))
-       # self.stack.push(3.01)
-       # self.assertEqual(self.stack.peek(),3.02)
+        self.stack.push(3)
+        self.assertEqual(self.stack.peek(),3)
 
 
     def test_is_empty(self):
